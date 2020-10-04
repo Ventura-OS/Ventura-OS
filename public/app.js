44630c7e5461bd2bb8c18b2a6b4431add56edd91
Imagineeeinc <imagineeelego@gmail.com>
2020-10-04 19:38:30 +0100

    added basic desktop
    Signed-off-by: Imagineeeinc <imagineeelego@gmail.com>

---
+++ public/app.js
@@ -0,0 +1,5 @@
+setInterval(function(){
+    document.getElementById("desk").style.height = cijs("sizeScreenH") - 188 + "px"
+}, 50);
+console.log(cijs("sizeScreenH") - 188)
+console.log(cijs("sizeScreenH"))
